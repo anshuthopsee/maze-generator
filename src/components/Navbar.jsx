ee2643d9659c37c4e0dc48555b78ae58f570ceda
anshuthopsee <thopseeanshu@gmail.com>
2023-05-14 04:22:14 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import AlgoSelect from "./AlgoSelect.jsx";
+
+export default function Navbar() {
+    return ( 
+        <div className="navbar">
+            <div className="header code">MAZE GENERATOR</div>
+            <AlgoSelect/>
+        </div>
+    );
+};
